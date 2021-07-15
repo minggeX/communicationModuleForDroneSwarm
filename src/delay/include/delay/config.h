b30ffd35ffae368c7ff936473989b74bd51d9d3b
minggeX <15603010928@163.com>
2021-07-15 17:07:20 +0800

    add client and server launch file

--- src/delay/include/delay/config.h
+++ src/delay/include/delay/config.h
@@ -1,3 +1,4 @@
+//server listen addr
 char server_addr_list[5][30]={
     "tcp://*:5555",
     "tcp://*:6666",
@@ -5,7 +6,9 @@ char server_addr_list[5][30]={
     "tcp://*:6060",
     "tcp://*:6565"
 };
-
+//client connect addr
+//replace the localhost to the actual server addr
+//keep the port coinstency with that in server_addr_list
 char connect_addr_list[5][30]={
     "tcp://localhost:5555",
     "tcp://localhost:6666",
@@ -13,6 +16,8 @@ char connect_addr_list[5][30]={
     "tcp://localhost:6060",
     "tcp://localhost:6565"
 };
-
+//client subscribe addr
+//replace the localhost to the actual server addr
 char subaddr[30]="tcp://localhost:7050";
+//server publish addr
 char pubaddr[30]="tcp://*:7050";
